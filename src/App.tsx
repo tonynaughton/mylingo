6646321dfdfe222babd087634a7f406bbddb938b
TonyN96 <tonynaughton1@gmail.com>
2024-04-26 16:43:43 +0200

    Login page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import { ChakraProvider } from "@chakra-ui/react";
-
-import Home from "./pages/home";
+import {
+  ChakraProvider,
+  Container,
+  VStack,
+  Flex,
+  Heading,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  Button
+} from "@chakra-ui/react";
 
 export default function App() {
   return (
     <ChakraProvider>
-      <Home />
+      <Container maxW="container.sm">
+        <Flex h="100vh" py={20}>
+          <VStack w="full" h="full" p={10} spacing={10} alignItems="center">
+            <VStack w="full" spacing={3}>
+              <Heading>Welcome to mylingo</Heading>
+              <Text>Please login to continue</Text>
+            </VStack>
+            <VStack w="full" spacing={5}>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input type="email"></Input>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input type="password"></Input>
+              </FormControl>
+              <Button w="full" size="lg">
+                Login
+              </Button>
+            </VStack>
+          </VStack>
+        </Flex>
+      </Container>
     </ChakraProvider>
   );
 }
--- src/pages/home.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home(): JSX.Element {
-  return <h1>Welcome to mylingo!</h1>;
-}
