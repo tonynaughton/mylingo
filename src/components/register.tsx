d86bfd3dfd82285511b70869526e2a9de06772d5
TonyN96 <tonynaughton1@gmail.com>
2024-05-10 14:10:27 +0200

    Add register form values

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,25 +1,37 @@
 import { VStack, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useState } from "react";
 
 export default function Register(): JSX.Element {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const onRegisterClick = async (): Promise<void> => {
+    await createUserWithEmailAndPassword(auth, email, password);
+  };
+
   return (
     <VStack w="full" spacing={3}>
       <FormControl>
         <FormLabel>Name</FormLabel>
-        <Input></Input>
+        <Input value={name} onChange={(e) => setName(e.target.value)}></Input>
       </FormControl>
       <FormControl>
         <FormLabel>Email</FormLabel>
-        <Input type="email"></Input>
+        <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></Input>
       </FormControl>
       <FormControl>
         <FormLabel>Password</FormLabel>
-        <Input type="password"></Input>
+        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
       </FormControl>
       <FormControl>
         <FormLabel>Repeat Password</FormLabel>
-        <Input type="password"></Input>
+        <Input type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)}></Input>
       </FormControl>
-      <Button w="full" size="lg">
+      <Button w="full" size="lg" onClick={onRegisterClick}>
         Register
       </Button>
     </VStack>
