3cb46dbcaccfd490b83afb9d7e4e21331504424b
TonyN96 <tonynaughton1@gmail.com>
2024-05-10 14:48:21 +0200

    Used react-hook-form for register form

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,39 +1,67 @@
-import { VStack, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { VStack, FormControl, FormLabel, Input, Button, FormErrorMessage } from "@chakra-ui/react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useForm } from "react-hook-form";
+
 import { auth } from "../firebase";
-import { useState } from "react";
+
+interface FormInput {
+  name: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
 
 export default function Register(): JSX.Element {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm<FormInput>();
 
-  const onRegisterClick = async (): Promise<void> => {
+  const onRegister = async (data: FormInput): Promise<void> => {
+    const { email, password } = data;
     await createUserWithEmailAndPassword(auth, email, password);
   };
 
   return (
-    <VStack w="full" spacing={3}>
-      <FormControl>
-        <FormLabel>Name</FormLabel>
-        <Input value={name} onChange={(e) => setName(e.target.value)}></Input>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Email</FormLabel>
-        <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></Input>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Repeat Password</FormLabel>
-        <Input type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)}></Input>
-      </FormControl>
-      <Button w="full" size="lg" onClick={onRegisterClick}>
-        Register
-      </Button>
-    </VStack>
+    <form onSubmit={handleSubmit(onRegister)} style={{ width: "100%" }}>
+      <VStack w="full" spacing={3}>
+        <FormControl isInvalid={!!errors.name}>
+          <FormLabel htmlFor="name">Name</FormLabel>
+          <Input
+            id="name"
+            {...register("name", {
+              required: "This is required",
+              minLength: { value: 4, message: "Minimum length should be 4" }
+            })}
+          />
+        </FormControl>
+        <FormControl isInvalid={!!errors.email}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            autoComplete="email"
+            id="email"
+            type="email"
+            {...register("email", { required: "This is required" })}
+          />
+        </FormControl>
+        <FormControl isInvalid={!!errors.password}>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input autoComplete="password" type="password" {...register("password", { required: "This is required" })} />
+        </FormControl>
+        <FormControl isInvalid={!!errors.repeatPassword}>
+          <FormLabel htmlFor="repeat-password">Repeat Password</FormLabel>
+          <Input
+            autoComplete="repeat-password"
+            type="password"
+            {...register("repeatPassword", { required: "This is required" })}
+          />
+        </FormControl>
+        <FormErrorMessage>{errors.name ? errors.name.message : ""}</FormErrorMessage>
+        <Button w="full" size="lg" isLoading={isSubmitting} type="submit">
+          Register
+        </Button>
+      </VStack>
+    </form>
   );
 }
